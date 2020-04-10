78fedf200729890bd09ba5f67575cfd77654ad03
1132601623@qq.com <wyf19971126>
2020-04-10 14:46:32 +0800

    新项目框架

---
+++ src/main/java/com/bigpractice/BigpracticeApplication.java
@@ -0,0 +1,13 @@
+package com.bigpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigpracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BigpracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bigpractice/BigpracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bigpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigpracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
